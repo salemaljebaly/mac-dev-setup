156f42fd62faeb71201e490ce0a68a309038388b
salemaljebaly <salemaljebaly@gmail.com>
2025-06-08 19:32:14 +0200

    feat: add flake.lock and improve darwin configuration handling in flake.nix

--- flake.nix
+++ flake.nix
@@ -27,47 +27,49 @@
     username = "YOUR_USERNAME"; # TODO: Update this
     hostname = "YOUR_HOSTNAME"; # TODO: Update this (run `scutil --get LocalHostName`)
     system = "aarch64-darwin"; # For M1/M4 Macs. Use "x86_64-darwin" for Intel
-    
-    # Helper function to create darwin configuration
-    mkDarwinConfig = hostname: username: darwin.lib.darwinSystem {
-      inherit system;
-      
-      modules = [
-        # System configuration
-        ./hosts/common/configuration.nix
-        ./hosts/${hostname}/configuration.nix
-        
-        # Homebrew integration
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            enableRosetta = true; # For M1/M4 Macs
-            user = username;
-            autoMigrate = true;
-          };
-        }
-        
-        # Home Manager integration
-        home-manager.darwinModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.${username} = import ./home/${username}/home.nix;
-            extraSpecialArgs = { inherit inputs username; };
-          };
-        }
-      ];
-      
-      specialArgs = { inherit inputs username; };
-    };
   in
   {
     # Build the system configuration
-    darwinConfigurations.${hostname} = mkDarwinConfig hostname username;
-    
-    # Convenience output for the default configuration
-    darwinConfigurations.default = mkDarwinConfig "default" username;
+    darwinConfigurations = {
+      ${hostname} = darwin.lib.darwinSystem {
+        inherit system;
+        
+        modules = [
+          # System configuration
+          ./hosts/common/configuration.nix
+          (if builtins.pathExists ./hosts/${hostname}/configuration.nix
+           then ./hosts/${hostname}/configuration.nix
+           else ./hosts/default/configuration.nix)
+          
+          # Homebrew integration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true; # For M1/M4 Macs
+              user = username;
+              autoMigrate = true;
+            };
+          }
+          
+          # Home Manager integration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${username} = import (
+                if builtins.pathExists ./home/${username}/home.nix
+                then ./home/${username}/home.nix
+                else ./home/default/home.nix
+              );
+              extraSpecialArgs = { inherit inputs username; };
+            };
+          }
+        ];
+        
+        specialArgs = { inherit inputs username; };
+      };
+    };
   };
 }
