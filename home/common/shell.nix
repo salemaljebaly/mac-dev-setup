9301e5410c992e496fb17f369f9bb55b8bf31f92
salemaljebaly <salemaljebaly@gmail.com>
2025-06-08 12:10:29 +0200

    feat: add zsh and starship prompt configuration

---
+++ home/common/shell.nix
@@ -0,0 +1,90 @@
+{ config, pkgs, ... }:
+
+{
+  # Zsh configuration
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    
+    shellAliases = {
+      # Navigation
+      ll = "eza -l -g -a --icons";
+      ls = "eza --icons";
+      lt = "eza --tree --level=2 --icons";
+      
+      # Git
+      g = "git";
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git log --oneline --graph";
+      
+      # Nix
+      rebuild = "darwin-rebuild switch --flake ~/.config/nix-config";
+      update = "cd ~/.config/nix-config && nix flake update && rebuild";
+      cleanup = "nix-collect-garbage -d";
+      
+      # Docker
+      d = "docker";
+      dc = "docker-compose";
+      
+      # Other
+      cat = "bat";
+      find = "fd";
+      grep = "rg";
+      vim = "nvim";
+    };
+    
+    initExtra = ''
+      # Custom prompt
+      eval "$(starship init zsh)"
+      
+      # Direnv hook
+      eval "$(direnv hook zsh)"
+      
+      # FZF integration
+      source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+      source ${pkgs.fzf}/share/fzf/completion.zsh
+      
+      # Custom functions
+      mkcd() {
+        mkdir -p "$1" && cd "$1"
+      }
+      
+      # Set editor
+      export EDITOR="nvim"
+      export VISUAL="nvim"
+    '';
+  };
+  
+  # Starship prompt
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+      directory = {
+        truncation_length = 3;
+        truncate_to_repo = true;
+      };
+      git_branch = {
+        symbol = " ";
+      };
+      git_status = {
+        ahead = "⇡\${count}";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+        behind = "⇣\${count}";
+      };
+      nix_shell = {
+        symbol = " ";
+        format = "via [$symbol$state( \\($name\\))]($style) ";
+      };
+    };
+  };
+}
