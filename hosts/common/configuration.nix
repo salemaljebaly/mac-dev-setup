7caaa7680aad97cf8f4ae0d5bf7379fe7791b546
salemaljebaly <salemaljebaly@gmail.com>
2025-06-08 19:08:13 +0200

    chore: add cache to packages and update README.md file

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -15,6 +15,17 @@
       
       # Users allowed to use Nix
       trusted-users = [ "@admin" username ];
+      
+      # Additional binary caches
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     
     # Garbage collection
