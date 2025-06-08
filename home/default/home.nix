7e3452baac48b5c90f48d91814928500256d4756
salemaljebaly <salemaljebaly@gmail.com>
2025-06-08 12:11:08 +0200

    chore: add default home-manager configuration

---
+++ home/default/home.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, username, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage
+  home = {
+    username = username;
+    homeDirectory = "/Users/${username}";
+    stateVersion = "23.11";
+  };
+  
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+  
+  # Import common configurations
+  imports = [
+    ../common/packages.nix
+    ../common/shell.nix
+    ../common/programs.nix
+  ];
+  
+  # Additional user-specific configuration can go here
+}
