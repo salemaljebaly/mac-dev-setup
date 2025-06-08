0b14fab8a8fe75c4ca8853e8e2c5e0df54fea69e
salemaljebaly <salemaljebaly@gmail.com>
2025-06-08 12:10:15 +0200

    feat: add Homebrew casks and App Store apps configuration

---
+++ modules/system/homebrew.nix
@@ -0,0 +1,56 @@
+{ config, ... }:
+
+{
+  # Homebrew configuration
+  homebrew = {
+    enable = true;
+    
+    # Auto update on rebuild
+    onActivation = {
+      autoUpdate = true;
+      # todo: needs to check
+      cleanup = "zap"; # Remove unlisted packages
+      upgrade = true;
+    };
+    
+    # Taps
+    taps = [
+      "homebrew/cask"
+      "homebrew/core"
+      "homebrew/services"
+    ];
+    
+    # Packages from Homebrew
+    brews = [
+      "mas" # Mac App Store CLI
+      "fastlane" # Mobile automation
+    ];
+    
+    # GUI Applications
+    casks = [
+      # Development
+      "android-studio"
+      "docker"
+      "cursor"
+      
+      # Communication
+      "slack"
+      "chatgpt"
+      
+      # Design
+      "figma"
+      
+      # VPN
+      "protonvpn"
+      "cloudflare-warp"
+      
+      # Utilities
+      "localsend"
+    ];
+    
+    # App Store Applications
+    masApps = {
+      "Xcode" = 497799835;
+    };
+  };
+}
